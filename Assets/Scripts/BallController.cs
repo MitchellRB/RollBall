f27f445442fde5b73f9fa6710b64b3c7657c1d99
MitchellRB <43767850+MitchellRB@users.noreply.github.com>
2020-08-28 10:58:52 +1000

    Added camera script and ball stub

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody), typeof(ConstantForce))]
+public class BallController : MonoBehaviour
+{
+    private Rigidbody rb;
+    private ConstantForce cForce;
+
+    private float forceScalar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        cForce = GetComponent<ConstantForce>();
+        forceScalar = cForce.force.magnitude;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = target.position + offset;
+    }
+}
